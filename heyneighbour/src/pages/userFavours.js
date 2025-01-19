c386db40de1511ba43654cd99b5d1fa81cdd05b4
Elmeri Hakkinen <60952417+hakkd@users.noreply.github.com>
2025-01-18 19:41:56 -0800

    created stubs

---
+++ heyneighbour/src/pages/userFavours.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const UserFavours = () => {
+    return (
+        <div>
+            <h1>User Favours</h1>
+            <p>This is the User Favours page.</p>
+        </div>
+    );
+};
+
+export default UserFavours;
