a7f16d4f915b0439c497f812c1286422b73084c1
Hedie <mistysky04@Hediescomp>
2025-01-19 01:19:20 -0800

    added signup page

--- heyneighbour/src/pages/SignUpPage.jsx
+++ heyneighbour/src/pages/SignUpPage.jsx
@@ -14,16 +14,16 @@ const SignUpPage = () => {
         				<div className={styles.groupChild} />
       			</div>
       			<div className={styles.component4}>
-        				<img className={styles.vectorIcon} alt="" src="Vector.svg" />
-        				<img className={styles.faceLIcon} alt="" src="Face L.svg" />
+        				<img className={styles.vectorIcon} alt="" src="../assets/red.svg" />
+						<img className={styles.faceLIcon} alt="" src="../assets/faceL.svg" />
       			</div>
       			<div className={styles.areYouHere}>Are you here to get help?</div>
         				<div className={styles.letsGetStarted}>Let’s get started...</div>
-        				<img className={styles.component6Icon} alt="" src="Component 6.svg" onClick={onComponentClick} />
-        				<img className={styles.registration1Child} alt="" src="Group 8.svg" />
-        				<img className={styles.component5Icon} alt="" src="Component 5.svg" onClick={onComponentClick} />
+        				<img className={styles.component6Icon} alt="" src="../assets/component6.svg" onClick={onComponentClick} />
+        				<img className={styles.registration1Child} alt="" src="../assets/faceL.svg" />
+        				<img className={styles.component5Icon} alt="" src="../assets/component5.svg" onClick={onComponentClick} />
         				<div className={styles.component13}>
-          					<img className={styles.textLogo} alt="" src={`Text & Logo.png`} />
+          					<img className={styles.textLogo} alt="" src="../assets/logo.svg" />
           					<div className={styles.testimonials}>TESTIMONIALS</div>
           					<div className={styles.aboutUs}>ABOUT US</div>
           					<div className={styles.myProfile}>MY PROFILE</div>
