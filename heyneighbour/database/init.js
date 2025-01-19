07b079fac89a4b2a4dae893b47e771f73e2de7d8
Elmeri Hakkinen <60952417+hakkd@users.noreply.github.com>
2025-01-18 17:37:42 -0800

    added uertype table

--- heyneighbour/database/init.js
+++ heyneighbour/database/init.js
@@ -13,4 +13,8 @@ import dbPromise from './sqlite.js';
   await db.exec('INSERT INTO reviews (userId, rating, comment, createdBy) VALUES (1, 5, "Great user", 2)');
   await db.exec('CREATE TABLE IF NOT EXISTS favours (id INTEGER PRIMARY KEY, userId INTEGER, title TEXT, description TEXT, createdBy INTEGER, status TEXT, date TEXT, assignedTo INTEGER, location TEXT)');
   await db.exec('INSERT INTO favours (userId, title, description, createdBy, status, date, assignedTo, location) VALUES (1, "Help with groceries", "I need help with groceries", 1, "open", "2021-01-01", 2, "A1A 1A1")');
+  await db.exec('DROP TABLE IF EXISTS userTypes');
+  await db.exec('CREATE TABLE IF NOT EXISTS userTypes (id INTEGER PRIMARY KEY, userId INTEGER, type TEXT)');
+  await db.exec('INSERT INTO userTypes (userId, type) VALUES (2, "help seeker")');
+  await db.exec('INSERT INTO userTypes (userId, type) VALUES (1, "helper")');
 })();
--- heyneighbour/src/app/pages/api/users.js
+++ heyneighbour/src/app/pages/api/users.js
@@ -7,8 +7,9 @@ export default async function handler(req, res) {
     const users = await db.all('SELECT * FROM users');
     res.status(200).json(users);
   } else if (req.method === 'POST') {
-    const { name, email } = req.body;
-    await db.run('INSERT INTO users (name, email) VALUES (?, ?)', [name, email]);
+    const { name, email, postalcode, usertype} = req.body;
+    await db.run('INSERT INTO users (name, email, postalcode) VALUES (?, ?, ?)', [name, email, postalcode]);
+    await db.run('INSERT INTO userTypes (userId, type) VALUES ((SELECT id FROM users WHERE email = ?), ?)', [email, usertype]);
     res.status(201).json({ message: 'User added' });
   } else {
     res.status(405).json({ message: 'Method not allowed' });
