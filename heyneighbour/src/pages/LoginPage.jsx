031a75a33befd36698743134b4d5f484bd4bc4cb
rachellechung <rc0704@students.cs.ubc.ca>
2025-01-19 01:14:36 -0800

    LoginPage complete but not really

---
+++ heyneighbour/src/pages/LoginPage.jsx
@@ -0,0 +1,36 @@
+const LoginPage = () => {
+
+    return (
+
+<div style={{width: '100%', height: '100%', position: 'relative', background: 'linear-gradient(0deg, #EF476F 0%, #EF476F 100%), linear-gradient(0deg, #FFD166 0%, #FFD166 100%), linear-gradient(0deg, #06D6A0 0%, #06D6A0 100%), linear-gradient(0deg, #0D0A0B 0%, #0D0A0B 100%), linear-gradient(0deg, #2660A4 0%, #2660A4 100%), linear-gradient(0deg, white 0%, white 100%)'}}>
+    <img style={{width: 338.50, height: 217, left: -26, top: -15, position: 'absolute'}} src="https://via.placeholder.com/339x217" />
+    <div style={{width: 219, height: 29, left: 224, top: 52, position: 'absolute', color: 'black', fontSize: 32, fontFamily: 'MS PGothic', fontWeight: '400', wordWrap: 'break-word'}}>TESTIMONIALS</div>
+    <div style={{width: 219, height: 29, left: 493, top: 52, position: 'absolute', color: 'black', fontSize: 32, fontFamily: 'MS PGothic', fontWeight: '400', wordWrap: 'break-word'}}>ABOUT US</div>
+    <div style={{left: 136, top: 181, position: 'absolute', color: 'black', fontSize: 48, fontFamily: 'Lalezar', fontWeight: '400', wordWrap: 'break-word'}}>Log-In:</div>
+    <div style={{width: 1001, height: 477, left: 280, top: 273, position: 'absolute', background: '#F0F0F0', borderRadius: 59}} />
+    <div style={{width: 238, height: 68, left: 367, top: 512, position: 'absolute', color: 'black', fontSize: 64, fontFamily: 'Myanmar Khyay', fontWeight: '400', wordWrap: 'break-word'}}>Email:</div>
+    <div style={{width: 740, height: 42, left: 452, top: 418, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 40, fontFamily: 'MS PGothic', fontWeight: '400', wordWrap: 'break-word'}}>Welcome back! Please enter your email:</div>
+    <div style={{width: 134, height: 134, left: 2686, top: 532, position: 'absolute', flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'flex-start', display: 'flex'}}>
+        <div style={{width: 89.33, height: 61.42, border: '4px #1E1E1E solid'}}></div>
+    </div>
+    <div style={{width: 167, height: 167, left: 1111, top: 666, position: 'absolute'}}>
+        <div style={{width: 167, height: 167, left: 0, top: 0, position: 'absolute', background: '#83B481', borderRadius: 9999}} />
+        <div style={{width: 134, height: 134, left: 17, top: 17, position: 'absolute', flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'flex-start', display: 'flex'}}>
+            <div style={{width: 89.33, height: 61.42, border: '4px #1E1E1E solid'}}></div>
+        </div>
+    </div>
+    <div style={{width: 438, height: 64, left: 743, top: 516, position: 'absolute', background: '#D9D9D9', borderRadius: 23}} />
+    <div style={{width: 576, height: 64, left: 605, top: 516, position: 'absolute', background: '#D9D9D9', borderRadius: 23}} />
+    <div style={{width: 760.81, height: 765.39, left: 908.09, top: 598.53, position: 'absolute', transform: 'rotate(-96.50deg)', transformOrigin: '0 0'}}>
+        <div style={{width: 517.45, height: 564.32, left: 370.52, top: 0, position: 'absolute', transform: 'rotate(41.04deg)', transformOrigin: '0 0', background: '#06D6A0'}}></div>
+        <div style={{width: 102, height: 87.23, left: 269, top: 238, position: 'absolute'}}>
+            <div style={{width: 21, height: 21, left: 0, top: 0, position: 'absolute', background: 'black', borderRadius: 9999}} />
+            <div style={{width: 21, height: 21, left: 81, top: 3, position: 'absolute', background: 'black', borderRadius: 9999}} />
+            <div style={{width: 74.55, height: 31.21, left: 13.43, top: 55, position: 'absolute', transform: 'rotate(0.78deg)', transformOrigin: '0 0', border: '7px black solid'}}></div>
+        </div>
+    </div>
+</div>
+    )
+};
+      			
+export default LoginPage;
